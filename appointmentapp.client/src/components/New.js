89a86ab2c68e27d0225d636d8c8e871085ff5a87
NahBeee <nahbee118@gmail.com>
2024-02-25 23:02:12 +1100

    Updating Home and New Appoinment Pages

--- appointmentapp.client/src/components/New.js
+++ appointmentapp.client/src/components/New.js
@@ -1,7 +1,37 @@
 export default function New (props){
     return(
-        <div>
-            New component
+        <div className="modal-container">
+            <div className="modal-title">New Appointment</div>
+
+            <div className="mt-15">
+                <label htmlFor="Title_n">Title</label> <br />
+                <input type="text" id="Title_n" maxLength={150} name="title" />
+                <span className="ms-10">0/150</span>
+            </div>
+
+            <div className="mt-15">
+                <label htmlFor="Description_n">Description</label> <br />
+                <textarea id="Description_n" maxLength={300} className="mt-5" name="description" cols={102} rows={10}></textarea> <br />
+                <span className="float-right me-10">0/300</span>
+            </div>
+
+            <div className="row mt-15">
+                <div>
+                    <label htmlFor="Address_n">Address</label> <br />
+                    <input type="text" id="Address_n" name="address" maxLength={100} />
+                </div>
+                <div className="ms-10">
+                    <label htmlFor="LevelOfImportance_n">Importance</label>
+                    <select name="levelOfImportance" id="LevelOfImportance_n" defaultValue={2}>
+                        <option value={5}>Very High</option>
+                        <option value={4}>High</option>
+                        <option value={1}>Very Low</option>
+                        <option value={0}>Low</option>
+                        <option value={3}>Medium</option>
+                        <option value={2}>Normal</option>
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
