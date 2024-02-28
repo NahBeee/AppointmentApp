5bcd03309ba0f3e26371c2b784dad4ad29310b71
NahBeee <nahbee118@gmail.com>
2024-02-28 21:46:55 +1100

    Fixing POST error

--- AppointmentApp.Server/Program.cs
+++ AppointmentApp.Server/Program.cs
@@ -8,13 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration
     .GetConnectionString("DefaultConnection")));
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseHsts();
+    //app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
